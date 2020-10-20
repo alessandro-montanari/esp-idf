ac819f8bd1e9835f8031399083010eb710916d1a
alessandro-montanari <alessandro.montanari@nokia-bell-labs.com>
2020-10-20 11:41:13 +0100

    Added support for gpio pin 20

--- components/driver/include/driver/gpio.h
+++ components/driver/include/driver/gpio.h
@@ -156,7 +156,7 @@ typedef enum {
     GPIO_NUM_17 = 17,   /*!< GPIO17, input and output */
     GPIO_NUM_18 = 18,   /*!< GPIO18, input and output */
     GPIO_NUM_19 = 19,   /*!< GPIO19, input and output */
-
+    GPIO_NUM_20 = 20,
     GPIO_NUM_21 = 21,   /*!< GPIO21, input and output */
     GPIO_NUM_22 = 22,   /*!< GPIO22, input and output */
     GPIO_NUM_23 = 23,   /*!< GPIO23, input and output */
